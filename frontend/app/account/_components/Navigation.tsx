3e113cde8f981c4e9269901be2f296e0e8ce2cc2
PetarGrabic <petarperograbic@gmail.com>
2025-11-08 19:47:11 +0100

    Dodane preostale rute i navigacija

---
+++ frontend/app/account/_components/Navigation.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Page = {
+  title: string;
+  path: `/${string}/${string}`;
+};
+
+const pages: Page[] = [
+  { title: "Login / Register", path: "/account/access" },
+  { title: "Profile", path: "/account/profile" },
+  { title: "Wishlist", path: "/account/wishlist" },
+];
+
+function processPage(page: Page, currentPath?: string) {
+  return (
+    <li key={page.path}>
+      <Link
+        href={page.path}
+        className={`p-2 rounded hover:bg-gray-200 ${
+          currentPath === page.path ? "font-bold border-b-3" : ""
+        }`}
+      >
+        {page.title}
+      </Link>
+    </li>
+  );
+}
+
+export default function Navigation() {
+  const currentPath = usePathname();
+  return (
+    <nav className="text-xl font-medium py-2 mt-5">
+      <ul className="flex gap-5 justify-center">
+        {pages.map((page) => processPage(page, currentPath))}
+      </ul>
+    </nav>
+  );
+}
---
+++ frontend/app/cart/checkout/page.tsx
@@ -0,0 +1,7 @@
+export default function Checkout() {
+  return (
+    <div className="text-center mt-5">
+      <h1 className="text-3xl font-medium">Checkout</h1>
+    </div>
+  );
+}
